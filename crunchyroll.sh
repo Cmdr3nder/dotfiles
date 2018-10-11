8c934e728519e1b4e97a04fc706c901082eb0d37
Andrew Bowers <andrew.bowers@agamecoder.com>
2018-10-10 20:33:02 -0700

    Cleaning up branching mistakes.

--- crunchyroll.sh
+++ crunchyroll.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-google-chrome --app=https://crunchyroll.com --class=CHROME_CRUNCHYROLL 2>/dev/null 1>/dev/null &
+if [ -z "$(ps aux | grep 'CHROME_CRUNCHYROLL' | grep -v 'grep')" ]; then
+    google-chrome --app=https://crunchyroll.com --class=CHROME_CRUNCHYROLL 2>/dev/null 1>/dev/null &
+fi
